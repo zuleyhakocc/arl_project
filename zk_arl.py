dc418d8c4e4fa8a200e72c35f6a9012559cde9f1
Züleyha Koç <78099138+zuleyhakocc@users.noreply.github.com>
2025-03-12 15:13:45 +0300

    Update zk_arl.py

--- zk_arl.py
+++ zk_arl.py
@@ -1,106 +1,71 @@
-################## ASSOCIATION RULE LEARNING RECOMMENDER ##################
+############################################
+# ASSOCIATION RULE LEARNING (BİRLİKTELİK KURALI ÖĞRENİMİ)
+############################################
+# Amacımız online retail II veri setine birliktelik analizi uygularak kullanıcılara ürün satın alma sürecinde ürün önermek.
 
-#### İş Problemi: sepet aşamasındaki kullanıcılara ürün önerisinde bulunmak.
+# Değişkenler
+# InvoiceNo: Fatura Numarası , Eğer bu kod C ile başlıyorsa işlemin iptal edildiğini ifade eder.
+# StockCode: Ürün kodu, Her bir ürün için eşsiz numara.
+# Description: Ürün İsmi
+# Quantity: Ürün Adedi, Faturlardaki ürünlerden kaçar tane satıldığını ifade etmektedir.
+# InvoiceDate: Fatura Tarihi
+# UnitPrice: Fatura fiyatı(sterlin)
+# CustomerID: Eşsiz müşteri numarası
+# Country: Ülke ismi
 
-###### Veri Seti Hikayesi ######
-
-# Online Retail II isimli veri seti İngiltere merkezli online bir satış mağazasının 01/12/2009 - 09/12/2011 tarihleri arasındaki satışlarını içeriyor.
-# Bu şirketin ürün kataloğunda hediyelik eşyalar yer alıyor. Promosyon ürünleri olarak da düşünülebilir.
-# Çoğu müşterisinin toptancı olduğu bilgisi de mevcut.
-
-####### NOT ###########
-# Aşağıda 3 farklı kullanıcının sepet bilgileri verilmiştir.
-# Bu sepet bilgilerine en uygun ürün önerisini yapınız.
-# Not: Ürün önerileri 1 tane ya da 1'den fazla olabilir.
-# Önemli not: Karar kurallarını 2010-2011 Germany müşterileri üzerinden türetiniz.
-# ▪ Kullanıcı 1 ürün id'si: 21987
-# ▪ Kullanıcı 2 ürün id'si: 23235
-# ▪ Kullanıcı 3 ürün id'si: 22747
 
 import pandas as pd
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 500)
-pd.set_option('display.expand_frame_repr', False)
-pip install mlxtend
+pd.set_option('display.max_columns', None) # bütün sütunları göster
+#pd.set_option('display.max_rows', None)   # bütün satırları göster
+pd.set_option('display.width', 500)        # yan yana 500 tane göster
+pd.set_option('display.expand_frame_repr', False)  # çıktının tek bir satırda olmasını sağlar.
 from mlxtend.frequent_patterns import apriori, association_rules
 
-########################### GÖREV 1 ####################################
+df_ = pd.read_excel("datasets/online_retail_II.xlsx", sheet_name="Year 2010-2011")
 
-# Veri Ön İşleme İşlemlerini Gerçekleştiriniz.
+df = df_.copy()
+df.head()
 
-# 2010-2011 verilerini seçiniz ve tüm veriyi ön işlemeden geçiriniz.
-# Germany seçimi sonraki basamakta yapılacaktır.
+######################################################
+# GÖREV 1:
+######################################################
+# Veri Ön İŞleme İşlemlerini Gerçekleştirme
 
-df = pd.read_excel("hafta3_python/online_retail_II.xlsx", sheet_name="Year 2010-2011")
-df_copy = df.copy()
-df.shape
-
-# veri setine bakalım
-def check_df(dataframe, head=5):
-    print("##################### Shape #####################")
-    print(dataframe.shape)
-    print("##################### Types #####################")
-    print(dataframe.dtypes)
-    print("##################### Head #####################")
-    print(dataframe.head(head))
-    print("##################### Tail #####################")
-    print(dataframe.tail(head))
-    print("##################### NA #####################")
-    print(dataframe.isnull().sum())
-    print("##################### Quantiles #####################")
-    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+###########################
+# Veri öN iŞLEME
+###########################
+from helpers.helpers import check_df, retail_data_prep
 check_df(df)
 
+###########################
+# Veri Ön İşleme Sonrası
+###########################
+df = retail_data_prep(df)   # veriyi temizler.
+check_df(df)
 
-def outlier_thresholds(dataframe, variable):
-    quartile1 = dataframe[variable].quantile(0.01)
-    quartile3 = dataframe[variable].quantile(0.99)
-    interquantile_range = quartile3 - quartile1
-    up_limit = quartile3 + 1.5 * interquantile_range
-    low_limit = quartile1 - 1.5 * interquantile_range
-    return low_limit, up_limit
-def replace_with_thresholds(dataframe, variable):
-    low_limit, up_limit = outlier_thresholds(dataframe, variable)
-    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
-    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+######################################################
+# GÖREV 2:
+######################################################
+# Germany müşterileri üzerinden birliktelik kuralları üretme.
 
-def retail_data_prep(dataframe):
-    dataframe.dropna(inplace=True)
-    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
-    dataframe = dataframe[dataframe["Quantity"] > 0]
-    dataframe = dataframe[dataframe["Price"] > 0]
-    replace_with_thresholds(dataframe, "Quantity")
-    replace_with_thresholds(dataframe, "Price")
-    return dataframe
-df = retail_data_prep(df)
-df.shape
+df_grmny = df[df['Country'] == "Germany"]
+check_df(df_grmny)   # indirgediğimiz veri setinin yapısal bilgileri
 
+# Faturalara sepet muamelesi yapılacak ve 1 sepette bu ürünlerden varsa 1 yoksa 0 yazılacak.
 
-########################### GÖREV 2 ####################################
+# 1. YOL
+def create_invoice_product_df(dataframe, id=False):
+    if id:
+        return dataframe.pivot_table(values='Quantity', index="Invoice", columns="StockCode", aggfunc="sum").fillna(0).applymap(lambda x: 1 if x > 0 else 0)
+    else:
+        return dataframe.pivot_table(values='Quantity', index="Invoice", columns="Description", aggfunc="sum").fillna(0).applymap(lambda x: 1 if x > 0 else 0)
 
-##### Germany müşterileri üzderinden birliktelik kuralları üretiniz #####
+# Bu matrix'ler seyrek matrix'ler olarak geçer. Bazı problemlerin temelini oluşturur.
+# Buradaki her bir invoice bir fatura bu faturalarda bu ürün var mı yok mu diyerek binary encoding ettik.
+grmnyi_inv_pro_df = create_invoice_product_df(df_grmny, id=True)
+grmnyi_inv_pro_df.head()
 
-#### ARL Veri Yapısının Hazırlanması (invoice-product matrix) ####
-# invoice-product kesişimleri 1 veya 0 olsun
-
-
-df_ge = df[df["Country"] == "Germany"]
-check_df(df_ge)
-df_ge.shape
-
-#hangi üründen kaç tane alındığı
-df_ge.groupby(["Invoice", "Description"]).agg({"Quantity":"sum"}).head(20)
-
-
-df_ge.groupby(["Invoice", "Description"]).agg({"Quantity":"sum"}).unstack().iloc[0:20, 0:20]
-
-#NaN --> 0
-df_ge.groupby(["Invoice", "Description"]).agg({"Quantity":"sum"}).unstack().fillna(0).iloc[0:20, 0:20]
-
-
-df_ge.groupby(["Invoice", "Description"]).agg({"Quantity":"sum"}).unstack().fillna(0).applymap(lambda x: 1 if x > 0 else 0).iloc[0:20, 0:20]
-
-# fonksiyonlaştırma
+# 2. YOL
 def create_invoice_product_df(dataframe, id=False):
     if id:
         return dataframe.groupby(['Invoice', "StockCode"])['Quantity'].sum().unstack().fillna(0). \
@@ -109,99 +74,105 @@ def create_invoice_product_df(dataframe, id=False):
         return dataframe.groupby(['Invoice', 'Description'])['Quantity'].sum().unstack().fillna(0). \
             applymap(lambda x: 1 if x > 0 else 0)
 
-# fonksiyonla yapılması:
-# id=True stock code'larını getirir, id=False descriptionlar'ı getirir. (aynı şey)
-ge_inv_pro_df = create_invoice_product_df(df_ge)
-ge_inv_pro_df = create_invoice_product_df(df_ge, id=True)
-ge_inv_pro_df.head()
+grmny_inv_pro_df = create_invoice_product_df(df_grmny, id=True)
+grmny_inv_pro_df.head()
 
-# stock code'un hangi ürün olduğunu görmek
-def check_id(dataframe, stock_code):
-    product_name = dataframe[dataframe["StockCode"] == stock_code][["Description"]].values[0].tolist()
-    print(product_name)
+# Denemelik
+df1 = df[df["Invoice"] == 536983]
+df1
 
-###### Birliktelik Kuralları #######
+df1.groupby(['Invoice', 'Description']).agg({"Quantity": "count"}).unstack()
 
-#apriori ile sadece support'u hesapla
-frequent_itemsets = apriori(ge_inv_pro_df, min_support=0.01, use_colnames=True)
-frequent_itemsets.sort_values(by="support", ascending=False).head()
+df[(df["StockCode"] == 22331) & (df["Invoice"] == 536983)]
 
-#association_rules ile diğer tüm metrikleri hesapla
-rules = association_rules(frequent_itemsets, metric="support", min_threshold=0.01)
-rules.sort_values(by="support", ascending=False).head()
+###########################
+# Birliktelik Kuralının Çıkarılması
+###########################
 
-#antecendents: önceki ürün.
-#consequents: sonraki ürün.
-#antecendents support: önceki ürünün tek başına olasılığı.
-#consequent support: sonraki ürünün tek başına olasılığı.
-#support: iki ürünün birlikte görülme olasılığı.
-#confidence: birisi alındığında diğerinin alınma olasılığı.
-#lift: biri alındığında diğerinin alınma olasılığının ne kadar kat arttığı.
+# Tüm olası ürün birlikteliklerinin olasılıkları için apriori fonksiyonu:
+grmny_frequent_itemsets = apriori(grmny_inv_pro_df, min_support=0.01, use_colnames=True)
 
-rules.sort_values(by="lift", ascending=False).head(100)
-rules.sort_values(by="confidence", ascending=False).head(100)
+# Bunlar ürünlerin tek başlarına gözükme olasılıklarıdır.
+grmny_frequent_itemsets.sort_values("support", ascending=False)   # Support değerine göre azalan bir şekilde sıralama işlemi.
+
+# Yukarda apriori ile sadece support'ları hesapladık.
+# association_rules ile diğer bütün metrikleri hesaplama işlemi:
+grmny_rules = association_rules(grmny_frequent_itemsets, metric="support", min_threshold=0.01)
+grmny_rules.sort_values("support", ascending=False).head()  # 2 ürünün birlikte gözükme olasılığına göre veri setinin sıralanması.
+
+# * antecedents: önceki ürün
+# * consequents: sonraki ürün
+# * antecedent_support: önceki ürünün tek başına olasılığı
+# * consequent_support: sonraki ürünün tek başına olasılığı
+# * support: 2 ürünün birlikte olasılığı
+# * confidence: birisi alındığında diğerinin alınma olasılığı
+# * leverage: kaldıraç etkisi, pratikte lift kullanılır çünkü leverage support'u yüksek olan değerlere öncelik verme eğilimi vardır.
+# * lift ise daha az sıklıklara rağmen gizlenmiş ilişkileri bulabilmektedir.
 
 
-########################### GÖREV 3 ####################################
-
-# ID'leri verilen ürünlerin isimleri nelerdir?
+######################################################
+# GÖREV 3:
+######################################################
+# ID'leri verilen ürünlerin isimleri neledir?
 # Kullanıcı 1 ürün id'si: 21987
 # Kullanıcı 2 ürün id'si: 23235
 # Kullanıcı 3 ürün id'si: 22747
 
-check_id(df_ge, 21987)
-check_id(df_ge, 23235)
-check_id(df_ge, 22747)
+df_grmny[df_grmny["StockCode"] ==  21987][["Description"]].values[0].tolist()  # Bu şekilde de gelir bu aşağıda fonksiyonlaştırıldı.
 
-########################### GÖREV 4 ####################################
+# hangi id hangi ürüne ait fonksiyonu
+def check_id(dataframe, stock_code):
+    product_name = dataframe[dataframe["StockCode"] == stock_code][["Description"]].values[0].tolist()
+    print(product_name)
 
+check_id(df_grmny, 21987)
+
+check_id(df_grmny, 23235)
+
+check_id(df_grmny, 22747)
+
+######################################################
+# GÖREV 4:
+######################################################
 # Sepetteki kullanıcılar için ürün önerisi yapınız.
 
-# Kullanıcı 1 örnek ürün id'si: 21987
+# Örnek olarak böyle bir kullanıcı geldi ve bu kullanıcı login oldu bir ürüne bakıyor ve sepetine ekledi dedik.
 
-product_id = 21987
-check_id(df, product_id)
+product_id = 23235
+check_id(df, product_id)  # ilk baştaki dataframe
 
-sorted_rules = rules.sort_values("lift", ascending=False)
+sorted_rules = grmny_rules.sort_values("lift", ascending=False)   # Kolaylık olsun diye ürünleri lift'e göre sıraladık.
+sorted_rules.shape
+sorted_rules.head()
 
-recommendation_list = []
-
-for i, product in enumerate(sorted_rules["antecedents"]):
-    for j in list(product):
-        if j == product_id:
-            recommendation_list.append(list(sorted_rules.iloc[i]["consequents"])[0])
-
-# 2 tane ürün önerisi geliyor
-recommendation_list[0:2]
-
-
+# rec_count: gözlem birimi sayısı
 def arl_recommender(rules_df, product_id, rec_count=1):
     sorted_rules = rules_df.sort_values("lift", ascending=False)
     recommendation_list = []
     for i, product in enumerate(sorted_rules["antecedents"]):
         for j in list(product):
-            if j == product_id:
+            if j == product_id and list(sorted_rules.iloc[i]["consequents"])[0] not in recommendation_list:
                 recommendation_list.append(list(sorted_rules.iloc[i]["consequents"])[0])
+
     return recommendation_list[0:rec_count]
 
-arl_recommender(rules, 21987, 1)
-#21086
+a = arl_recommender(grmny_rules, 23235, 2)   # Tekrar edenleri içine katılmadı fonksiyon içerisinde elendi.
+a
 
-arl_recommender(rules, 23235, 1)
-#23244
+b = arl_recommender(grmny_rules, 21987, 4)
+b
 
-arl_recommender(rules, 22747, 1)
-#22745
+c = arl_recommender(grmny_rules, 22747, 3)
+c
 
-########################### GÖREV 5 ####################################
+check_id(df, a[1])
+######################################################
+# GÖREV 5:
+######################################################
+# Önerilen ürünlerin isimleri nelerdir?
 
-# Ürünlerin isimleri nelerdir?
-
-check_id(df,21989)
-# PACK OF 20 SKULL PAPER NAPKINS
-
-check_id(df,23243)
-# SET OF TEA COFFEE SUGAR TINS PANTRY
-
-check_id(df,22745)
-# POPPY'S PLAYHOUSE BEDROOM
+list = [a, b, c]
+for i, x in enumerate(list):
+    print(F" {x[1]} ".center(50, "*"))
+    xx = df_grmny[df_grmny["StockCode"] == x[1]][["Description"]].values[0]
+    print(xx)
